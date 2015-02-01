423014af5ce505ff716d6ccb239e800c27d45341
yunlzheng <yunl.zheng@gmail.com>
2015-02-01 21:05:23 +0800

    update template

---
+++ power/api/demo.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+#!/usr/bin/python
+
+from flask.views import MethodView
+
+class DemoAPI(MethodView):
+
+    def get(self):
+        return ""
+
+    def post(self):
+        return ""
+
--- server.py
+++ server.py
@@ -4,7 +4,11 @@
 from flask import Flask
 from flask import render_template
 
+from power.api.demo import DemoAPI
+
 app = Flask(__name__)
+
+app.add_url_rule('/api/demo', view_func=DemoAPI.as_view('demos'))
 
 @app.route('/', methods=['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
 def hello_world():
@@ -15,20 +19,23 @@ def hello():
     return 'Hello World'
 
 @app.route('/roles', methods=['GET', 'POST'])
-def show_roles():
-    # show the user profile for that user
-    return "platform roles"
-
 @app.route('/roles/<uuid>', methods=['GET', 'POST'])
-def show_role(uuid):
-    return "platform roles {0}".format(uuid)
+def roles(uuid=None):
+    if uuid:
+        return "platform host {0}".format(uuid)
+    return render_template('roles.html')
+
 
 @app.route('/hosts', methods=['GET', 'POST'])
 @app.route('/hosts/<uuid>', methods=['GET', 'POST'])
 def show_hosts(uuid=None):
     if uuid:
         return "platform host {0}".format(uuid)
-    return "platform roles"
+    return render_template('hosts.html')
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
